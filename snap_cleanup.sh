ee41be655f02565f531a071f1344727a40600372
Ubuntu <ubuntu@ip-172-31-44-89.us-east-2.compute.internal>
2025-12-10 02:25:22 +0000

    se actualizó snap para que borre .snap comprimidos y en caché

--- snap_cleanup.sh
+++ snap_cleanup.sh
@@ -1,13 +1,53 @@
 #!/bin/bash
 
-# Limpieza de snaps deshabilitados
-
 echo "=== Snap cleanup started: $(date) ==="
 
-# Listar snaps deshabilitados (nombre + revisión)
+##############################################
+# 1. Eliminar revisiones deshabilitadas
+##############################################
+
+echo "[1/4] Removing disabled snaps..."
 snap list --all | awk '/disabled/{print $1 " " $3}' | while read name revision; do
-    echo "Removing disabled snap: $name (revision $revision)"
+    echo " - Removing disabled snap: $name (rev $revision)"
     sudo snap remove "$name" --revision="$revision"
 done
 
-echo "=== Snap cleanup finished: $(date) ==="
+##############################################
+# 2. Limpiar caché local de Snapd
+##############################################
+
+echo "[2/4] Cleaning snapd cache..."
+sudo rm -f /var/lib/snapd/cache/* 2>/dev/null
+
+##############################################
+# 3. Identificar snaps activos (montados)
+##############################################
+
+echo "[3/4] Detecting active mounted snaps..."
+
+mounted_snaps=$(mount | grep snap | awk -F'/' '{print $4 "_" $5}' | sed 's/\/.*//' )
+
+##############################################
+# 4. Identificar y borrar archivos .snap huérfanos
+##############################################
+
+echo "[4/4] Removing orphan .snap files..."
+
+for snapfile in /var/lib/snapd/snaps/*.snap; do
+    filename=$(basename "$snapfile")
+    snapname=$(echo "$filename" | cut -d'_' -f1)
+    rev=$(echo "$filename" | cut -d'_' -f2 | cut -d'.' -f1)
+
+    # Formato usado por mount para compararlo: <nombre>_<rev>
+    mounted="${snapname}_${rev}"
+
+    # Si no está montado, es huérfano => SEGURO PARA BORRAR
+    if ! echo "$mounted_snaps" | grep -q "$mounted"; then
+        echo " - Removing orphan snap file: $filename"
+        sudo rm -f "$snapfile"
+    else
+        echo " - Keeping active snap: $filename"
+    fi
+done
+
+echo "=== Snap cleanup completed: $(date) ==="
